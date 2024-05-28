5cd6fc27bf2079189983b1c075486565e8bdf9a8
Caiij404 <1061690773@qq.com>
2024-05-28 15:11:41 +0800

    p1 你好，三角形

--- src/P1/main1.cpp
+++ src/P1/main1.cpp
@@ -1,31 +1,38 @@
-// 你好，三角形
+// 你好，窗口
+#include <random>
+#include <iostream>
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
-#include <iostream>
+// 定义函数
+void frameBuffer_Size_CB(GLFWwindow *window, int w, int h)
+{
+    glViewport(0, 0, w, h);
+    std::cout << "w: " << w << "   " << "h: " << h << std::endl;
+}
 
-void framebuffer_size_callback(GLFWwindow *window, int width, int height);
-void processInput(GLFWwindow *window);
-
-const unsigned int SCR_WIDTH = 800;
-const unsigned int SCR_HEIGHT = 600;
+// 声明函数
+void keyDown(GLFWwindow *window);
 
 int main()
 {
     glfwInit();
+    // 主版本号3
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    // 次版本号3
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    // 核心模式
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-    GLFWwindow *window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL", NULL, NULL);
+    GLFWwindow *window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
     if (window == NULL)
     {
         std::cout << "Failed to create GLFW window" << std::endl;
         glfwTerminate();
         return -1;
     }
+
     glfwMakeContextCurrent(window);
-    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
     if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
     {
@@ -33,26 +40,40 @@ int main()
         return -1;
     }
 
+    glfwSetFramebufferSizeCallback(window, frameBuffer_Size_CB);
+
     while (!glfwWindowShouldClose(window))
     {
-        glClearColor(0.2, 0.1, 0.4, 1.0);
-        glClear(GL_COLOR_BUFFER_BIT);
-        processInput(window);
-        glfwSwapBuffers(window);
+        // 检查按键输入
+        keyDown(window);
+
+        // 检查并调用事件，交换缓冲 （没有则会崩溃）
         glfwPollEvents();
+        glfwSwapBuffers(window);
+
+        std::random_device rd;
+        std::mt19937 gen(rd());
+        std::uniform_real_distribution<> dis(0.0, 1.0);
+
+        float r = (float)sin(glfwGetTime()) / 2.0f + 0.5f;
+        float g = (float)cos(glfwGetTime()) / 2.0f + 0.5f;
+        // 清空屏幕颜色缓存，不然只会显示黑色
+        glClear(GL_COLOR_BUFFER_BIT);
+        glClearColor(r, g, 0.0, 1.0f);
+
+        // 为啥每次输出都是固定的
+        // std::cout << dis(gen) << " " << dis(gen) << " " << dis(gen)<< std::endl;
     }
 
     glfwTerminate();
     return 0;
 }
 
-void processInput(GLFWwindow *window)
+void keyDown(GLFWwindow *window)
 {
+    // 绑定快捷键，escape退出程序
     if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+    {
         glfwSetWindowShouldClose(window, true);
-}
-
-void framebuffer_size_callback(GLFWwindow *window, int width, int height)
-{
-    glViewport(0, 0, width, height);
+    }
 }
