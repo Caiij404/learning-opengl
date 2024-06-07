2939d3874aa6b5c118777173541e2a89aeb81856
Caiij404 <1061690773@qq.com>
2024-06-07 17:56:28 +0800

    07_load_texture

--- src/07_load_texture/shader/fragment.glsl
+++ src/07_load_texture/shader/fragment.glsl
@@ -6,7 +6,7 @@ in vec2 texCoord;
 // 采样器 Sampler, 后缀1D/2D/3D是类型
 uniform sampler2D ourTexture;
 // 片段着色器也应该能使用采样器,因此在这里定义一个uniform的2D采样器
-// 在main中,绑定纹理glBindTexture,就会自动把纹理
+// 在main中,绑定纹理glBindTexture,就会自动把纹理赋值给片段着色器的采样器
 
 void main() {
     // FragColor = vec4(ourColor, 1.0);
