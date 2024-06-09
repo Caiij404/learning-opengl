79af4f12747049124459d96c19034d5c02e679f1
Caiij404 <1061690773@qq.com>
2024-06-10 00:53:54 +0800

    glm_io.hpp

---
+++ include/tool/glm_io.hpp
@@ -0,0 +1,32 @@
+#ifndef GLM_IO_H
+#define GLM_IO_H
+#include <iostream>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+std::ostream &operator<<(std::ostream &os, const glm::mat4 &matrix)
+{
+    // 将矩阵转换为指向float的指针
+    const float *pMatrix = (const float *)glm::value_ptr(matrix);
+
+    for (int i = 0; i < 4; ++i)
+    {
+        for (int j = 0; j < 4; ++j)
+        {
+            float t = abs(pMatrix[j * 4 + i]) < 1e-7 ? 0 : pMatrix[j * 4 + i];
+            os << t << " ";
+        }
+        os << std::endl;
+    }
+    std::cout << "----------------------------------" << std::endl;
+    return os;
+}
+
+std::ostream &operator<<(std::ostream &os, const glm::vec4 &v4)
+{
+    std::cout << "glm::vec4 ---- " << "X: " << v4.x << " Y: " << v4.y << " Z: " << v4.z << " W: " << v4.w << std::endl;
+}
+
+
+
+#endif
