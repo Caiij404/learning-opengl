3a860cef0a9f4e05b4915ee324cb47ddf9aab8a7
Caiij404 <1061690773@qq.com>
2024-07-10 16:32:15 +0800

    31_blending

--- include/tool/mySpace.h
+++ include/tool/mySpace.h
@@ -51,7 +51,7 @@ namespace mySpace
         glGenTextures(1, &textureID);
 
         // 图像y轴翻转
-        stbi_set_flip_vertically_on_load(true);
+        // stbi_set_flip_vertically_on_load();
         int width, height, nrComponents;
         unsigned char *data = stbi_load(path, &width, &height, &nrComponents, 0);
         if (data)
@@ -68,8 +68,16 @@ namespace mySpace
             glTexImage2D(GL_TEXTURE_2D, 0, format, width, height, 0, format, GL_UNSIGNED_BYTE, data);
             glGenerateMipmap(GL_TEXTURE_2D);
 
-            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+            if (nrComponents == 4)
+            {
+                glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+                glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+            }
+            else
+            {
+                glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+                glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+            }
             glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
             glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 
